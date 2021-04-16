d9eb2bbeb068ecc5496c38c40eaf8521d56bbb06
Marco Daniël Leguijt <m.leguijt@outlook.com>
2021-04-16 23:06:10 +0200

    init

---
+++ wp-graphql-acf-post-type-selector.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Plugin Name: ACF Post Type Selector WPGraphQL Extension
+ * Plugin URI: https://mddd.nl
+ * Description: Add ACF post type selector field to WPGraphQL.
+ * Author: M.D. Leguijt
+ * Author URI: https://mddd.nl
+ * Version: 1.0.0
+ */
+
+if (!defined('ABSPATH')) {
+  exit;
+}
+
+add_filter('wpgraphql_acf_supported_fields', function($supported_fields) {
+	$supported_fields[] = 'post_type_selector';
+
+	return $supported_fields;
+});
+
+add_filter( 'wpgraphql_acf_register_graphql_field', function($field_config, $type_name, $field_name, $config) {
+	$acf_field = isset( $config['acf_field'] ) ? $config['acf_field'] : null;
+	$acf_type  = isset( $acf_field['type'] ) ? $acf_field['type'] : null;
+
+	if( !$acf_field ) {
+		return $field_config;
+	} 
+
+	// ignore all other field types
+	if( $acf_type !== 'post_type_selector' ) {
+			return $field_config;
+	}
+
+	// define data type
+	$field_config['type'] = 'String';
+
+	// add resolver
+	$field_config['resolve'] = function( $root ) use ( $acf_field ) {
+		// when field is used in WP_Post and is top-level field (not nested in repeater, flexible content etc.)
+		if( $root->ID ) {
+				$value = get_field( $acf_field['key'], $root->ID, false );
+
+		// when field is used in WP_Post and is nested in repeater, flexible content etc. ...
+		} elseif( array_key_exists( $acf_field['key'], $root ) ) {
+				$value = $root[$acf_field['key']];
+		} 
+
+		return !empty( $value ) ? $value : null;
+	};
+
+	return $field_config;
+}, 10, 4 );
